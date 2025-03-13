d120c81e1fea9155594b14a33bb42a50d7daf220
SWAAFIYAH <swaafiyah9@gmail.com>
2025-03-13 09:18:13 +0300

    updates

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
 number1 = int(input("please input first number:"))
 number2 = int(input("please enter second number:"))
 result = number1 + number2
-print(result)
+print(f"{number1} + {number2} = {result}")
